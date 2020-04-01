b444748c139f51f79a1d41b6256e48807e3baee1
JEANYPC\Gebruiker <jeanydevries@hotmail.com>
2020-04-01 09:36:47 +0200

    animation for left arm

--- SecurityAgainstVirus/Assets/Scripts/Hand.cs
+++ SecurityAgainstVirus/Assets/Scripts/Hand.cs
@@ -2,5 +2,26 @@
 
 public class Hand : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Transform leftHand, rightHand;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetBool("LeftIsPunching", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            animator.SetBool("RightIsPunching", true);
+        }
+    }
 }
