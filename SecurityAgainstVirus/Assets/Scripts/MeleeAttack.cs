65942ab93f009063653d79ff6fe5c026072a5459
JEANYPC\Gebruiker <jeanydevries@hotmail.com>
2020-04-03 23:00:18 +0200

    Better restoration + skybox added

--- SecurityAgainstVirus/Assets/Scripts/MeleeAttack.cs
+++ SecurityAgainstVirus/Assets/Scripts/MeleeAttack.cs
@@ -26,7 +26,22 @@ public class MeleeAttack : MonoBehaviour
         Rigidbody rigidbody = virus.GetComponent<Rigidbody>();
         rigidbody.velocity = Vector3.zero;
         rigidbody.angularVelocity = Vector3.zero;
-        rigidbody.rotation = Quaternion.Euler(Vector3.zero);
-        virus.transform.LookAt(transform.position);
+        //rigidbody.rotation = Quaternion.Euler(Vector3.zero);
+
+        Vector3 targetForward = (virus.transform.position - transform.position);
+        targetForward.Normalize();
+        Quaternion targetRotation = Quaternion.LookRotation(targetForward);
+        Quaternion currentRotation = virus.transform.rotation;
+
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime / 1.0f;
+            virus.transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, t);
+
+            yield return null;
+        }
+        yield return null;
+        //virus.transform.LookAt(transform.position);
     }
 }
