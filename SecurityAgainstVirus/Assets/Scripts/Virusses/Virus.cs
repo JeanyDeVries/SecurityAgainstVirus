6906494a5c5c7b76f06350bd641ef1dfe018caa8
JEANYPC\Gebruiker <jeanydevries@hotmail.com>
2020-04-15 12:46:30 +0200

    Error fixed

--- SecurityAgainstVirus/Assets/Scripts/Virusses/Virus.cs
+++ SecurityAgainstVirus/Assets/Scripts/Virusses/Virus.cs
@@ -24,7 +24,6 @@ public class Virus : MonoBehaviour
 
     public virtual void Update()
     {
-        ObstacleAvoidance();
         checkForCollision();
     }
 
@@ -43,6 +42,9 @@ public class Virus : MonoBehaviour
 
     public virtual void Follow(Transform target)
     {
+        if (isDying) return;
+        navMeshAgent.SetDestination(player.transform.position);
+
         float distance =
             Vector3.Distance(transform.position, target.position);
         if (distance <= properties.attackRange)
@@ -51,52 +53,6 @@ public class Virus : MonoBehaviour
             CheckIfInAttackDistance(target);
             return;
         }
-
-        if (isDying) return;
-        navMeshAgent.SetDestination(player.transform.position);
-        //Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-        //Quaternion currentRotation = transform.rotation;
-        //transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, properties.turnSpeed * Time.deltaTime);
-
-        //transform.Translate(Vector3.forward * properties.movementSpeed * Time.deltaTime);
-    }
-
-    public virtual void ObstacleAvoidance()
-    {
-        lookDirection = (player.transform.position - transform.position).normalized;
-
-
-
-        /*float shoulderMultiplier = 0.5f;
-        float rayDistance = 10;
-        Vector3 leftRayPos = transform.position - (transform.right * shoulderMultiplier);
-        Vector3 rightRayPos = transform.position + (transform.right * shoulderMultiplier);
-
-
-        if (Physics.Raycast(leftRayPos, transform.forward, out hit, rayDistance))
-        {
-            Debug.DrawLine(transform.position, hit.point, Color.red);
-            if(hit.transform.gameObject.tag == "Firewall" || hit.transform.gameObject.tag == "Obstacle")
-            {
-                Debug.Log("Avoiding left");
-                lookDirection += hit.normal * 20.0f;
-            }
-        }
-        else if (Physics.Raycast(rightRayPos, transform.forward, out hit, rayDistance))
-        {
-            Debug.DrawLine(transform.position, hit.point, Color.red);
-            if(hit.transform.gameObject.tag == "Firewall" || hit.transform.gameObject.tag == "Obstacle")
-            {
-                Debug.Log("Avoiding right");
-                lookDirection += hit.normal * 20.0f;
-            }
-        }
-        else
-        {
-            Debug.DrawLine(leftRayPos, transform.forward * rayDistance, Color.yellow);
-            Debug.DrawLine(rightRayPos, transform.forward * rayDistance, Color.yellow);
-        }
-        */
     }
 
     public virtual void CheckIfInAttackDistance(Transform target)
