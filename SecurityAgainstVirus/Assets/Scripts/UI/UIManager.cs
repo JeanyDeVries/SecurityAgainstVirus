ca046b254f2262d0082755d906d3bbed03e3d208
JEANYPC\Gebruiker <jeanydevries@hotmail.com>
2020-05-19 10:37:24 +0200

    Credits added to the game

--- SecurityAgainstVirus/Assets/Scripts/UI/UIManager.cs
+++ SecurityAgainstVirus/Assets/Scripts/UI/UIManager.cs
@@ -3,21 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject creditsUI;
+
     public void PlayGame()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("Game");
     }
 
-    public void GoToOptions()
-    {
-
-    }
-
     public void GoToMenu()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToCredits()
+    {
+        creditsUI.SetActive(true);
+    }
+
+    public void EscapeCredits()
+    {
+        creditsUI.SetActive(false);
     }
 
     public void QuitGame()
